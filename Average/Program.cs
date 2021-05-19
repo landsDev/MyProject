af3dcb0a1577a846e0fd3c8ac6641ddaf357391a
Landshtreicher <landshtreicher@gmail.com>
2021-05-19 13:32:26 +0300

    add my projects

---
+++ Average/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+
+            Console.Write("Please, enter the first number: ");
+            while (true)
+            {
+
+                if (double.TryParse(Console.ReadLine(), out firstValue))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("It isn't number... Try again.");
+                }
+            }
+            Console.Write("Please, enter the second number: ");
+            while(true)
+            {
+                try
+                {
+                    secondValue = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("It isn't number... Try again.");
+                }
+            }
+            Console.WriteLine($"The average numbers {firstValue} and {secondValue} " +
+                $"is {(firstValue + secondValue) / 2}");
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите дробное число: ");
+                // разделитель в дробных числах для разных регионов
+                NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+                {
+                    NumberDecimalSeparator = ".",
+                };
+                try
+                {
+                    double userIput = double.Parse(Console.ReadLine(), numberFormatInfo);
+                    Console.WriteLine("OK, вы ввели " + userIput);
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неверный формат ввода!");
+                }
+            }
+        }
+    }
+}
